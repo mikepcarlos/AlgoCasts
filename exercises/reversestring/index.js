faa7923d9208bb6a5e6b7e4b40a49f953ca46e72
mikepcarlos <michaelpcarlos@gmail.com>
2019-01-29 10:40:14 -0500

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,14 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i]
+  }
+  return result
+}
+
+reverse('apple');
 
 module.exports = reverse;
